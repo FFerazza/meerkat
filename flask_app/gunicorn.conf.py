ac18c14c64eeb8efccfa8226aed94432c7d95b9d
Francesco Ferazza <francesco.ferazza@gmail.com>
2023-07-21 17:35:30 +0200

    higher timeout

--- flask_app/gunicorn.conf.py
+++ flask_app/gunicorn.conf.py
@@ -3,7 +3,7 @@ from os import environ
 
 bind = '0.0.0.0:' + environ.get('PORT', '8003')
 max_requests = 1000
-timeout = 300
+timeout = 600
 threads = 4
 workers = (2 * cpu_count()) + 1
 wsgi_app = 'wsgi:app'
